92fda35c5f225bdedae1b8ce838b2ebf9a181d1f
Max McDonough <max.mcdonough@gmail.com>
2022-07-20 11:17:04 -0400

    Fixed variables and imports, added routes

--- server.js
+++ server.js
@@ -1,25 +1,36 @@
-// Import Modules
+// Import Modules and files
 import express from "express";
 import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import dbNotes from './db/db.json';
 
 // Set PORT variable
 const PORT = process.env.PORT || 3001;
 
-// Set Express variable
+// Set variables
 const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-// Set public folders on port
+// Set public folder on port
 app.use(express.static('public'));
 
-// // GET Route for homepage
-// app.get('/', (req, res) =>
-//   res.sendFile(path.join(__dirname, 'index.html'))
-// );
+// GET Route for homepage
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
 
-// // GET Route for notes page
-// app.get('/', (req, res) =>
-//   res.sendFile(path.join(__dirname, 'notes.html'))
-// );
+// GET Route for notes page
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+// GET API Routes
+app.get('/api/notes', (req. res) => {
+    res.json(dbNotes);
+});
 
 // Set listening on PORT
 app.listen(PORT, () =>
